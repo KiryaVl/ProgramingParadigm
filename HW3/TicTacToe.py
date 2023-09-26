5b3aa3f0d37718eaeda9408ded88eac9441b3c30
Kirya <rabbitnya@gmail.com>
2023-09-26 19:55:21 +0300

    HW3

--- HW3/TicTacToe.py
+++ HW3/TicTacToe.py
@@ -13,55 +13,76 @@
 # усмотрение. Главное, чтобы в игру можно было поиграть через
 # терминал с вашего компьютера
 
-def tic_tac_toe():
-    board = [[' ' for _ in range(3)] for _ in range(3)]
-    current_player = 'X'
+def displayBoard(board):
+    print(f"\n {board[2][0]} | {board[2][1]} | {board[2][2]} ")
+    print("-----------")
+    print(f" {board[1][0]} | {board[1][1]} | {board[1][2]} ")
+    print("-----------")
+    print(f" {board[0][0]} | {board[0][1]} | {board[0][2]} ")
+
+def printBoardNumbers():
+    print("\n 7 | 8 | 9")
+    print("-----------")
+    print(" 4 | 5 | 6")
+    print("-----------")
+    print(" 1 | 2 | 3\n")
+
+def checkWinner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    for col in range(3):
+        if all(board[i][col] == player for i in range(3)):
+            return True
+
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
+def isBoardFull(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+def getPlayerMove(board, player):
+    while True:
+        try:
+            move = input(f"Игрок {player}, введите номер поля (1-9): ")
+            position = int(move) - 1
+            row = position // 3
+            col = position % 3
+            if board[row][col] != ' ' or not 0 <= position <= 8:
+                print("Некорректный ход! Попробуйте снова.")
+                continue
+            return row, col
+        except ValueError:
+            print("Некорректный ввод! Введите номер поля от 1 до 9.")
+
+def ticTacToe():
+    board = [[' ' for j in range(3)] for i in range(3)]
+    currentPlayer = 'X'
+
+    print("Добро пожаловать в игру Крестики-нолики!")
+    print("Ниже представлена нумерация полей:")
+    printBoardNumbers()
 
     while True:
-        print("   7 | 8 | 9")
-        print("   ---------")
-        print("   4 | 5 | 6")
-        print("   ---------")
-        print("   1 | 2 | 3")
-        print(f"\n _____________\n")
+        row, col = getPlayerMove(board, currentPlayer)
+        board[row][col] = currentPlayer
 
-        for i in range(3):
-            print("|", end="")
-            for j in range(3):
-                print(f" {board[i][j]} |", end="")
-            print()
-            if i < 2:
-                print("------------")
-
-        while True:
-            try:
-                move = input(f"Игрок {current_player}, введите номер поля (7-9, 4-6, 1-3): ")
-                if move not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                    print("Некорректный ход! Попробуйте снова.")
-                    continue
-                position = int(move)
-                position_map = {7: (0, 0), 8: (0, 1), 9: (0, 2), 4: (1, 0), 5: (1, 1), 6: (1, 2), 1: (2, 0), 2: (2, 1), 3: (2, 2)}
-                row, col = position_map[position]
-                if board[row][col] != ' ':
-                    print("Некорректный ход! Это поле уже занято.")
-                    continue
-                board[row][col] = current_player
-                break
-            except ValueError:
-                print("Некорректный ввод! Введите номер поля от 7 до 9, 4 до 6 или 1 до 3.")
-
-        if any(all(cell == current_player for cell in row) for row in board) or \
-                any(all(board[i][j] == current_player for i in range(3)) for j in range(3)) or \
-                all(board[i][i] == current_player for i in range(3)) or \
-                all(board[i][2 - i] == current_player for i in range(3)):
-            print(f"Игрок {current_player} победил!")
+        if checkWinner(board, currentPlayer):
+            displayBoard(board)
+            print(f"Игрок {currentPlayer} победил!")
             break
-        elif all(cell != ' ' for row in board for cell in row):
+        elif isBoardFull(board):
+            displayBoard(board)
             print("Ничья! Никто не победил.")
             break
         else:
-            current_player = 'O' if current_player == 'X' else 'X'
+            displayBoard(board)
+            currentPlayer = 'O' if currentPlayer == 'X' else 'X'
+
 
 if __name__ == "__main__":
-    tic_tac_toe()
-
+    printBoardNumbers()
+    ticTacToe()
